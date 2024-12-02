4c99fc84cb94372865dee04f9a199d70b848856f
tommo43 <37453991+tommo43@users.noreply.github.com>
2024-12-02 14:59:16 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,9 +5,11 @@ if len(sys.argv) > 1:
     # Read the diff from the provided file
     with open(sys.argv[1], 'r') as f:
         diff = f.read()
+    print("1")
 else:
     # Read the diff from standard input
     diff = sys.stdin.read()
+    print("2")
 
 # Print the diff (or you could process it further)
 print("hi")
