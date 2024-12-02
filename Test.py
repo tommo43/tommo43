b8b3e17a4a54be30706950a52d78a1e9678064bd
tommo43 <37453991+tommo43@users.noreply.github.com>
2024-12-02 14:50:48 +0000

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,13 @@
+import sys
+
+# Check if the diff is provided via file or standard input
+if len(sys.argv) > 1:
+    # Read the diff from the provided file
+    with open(sys.argv[1], 'r') as f:
+        diff = f.read()
+else:
+    # Read the diff from standard input
+    diff = sys.stdin.read()
+
+# Print the diff (or you could process it further)
+print(diff)
