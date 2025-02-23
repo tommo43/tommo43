5296225bf6dda21c7cc915ab0d00b714b76eac72
tommo43 <37453991+tommo43@users.noreply.github.com>
2025-02-23 20:24:07 +0000

    Update generate_message.py

--- generate_message.py
+++ generate_message.py
@@ -5,7 +5,7 @@ with open(sys.argv[1], 'r') as f:
     diff = f.read()
 
 # TODO: Replace this with logic to generate a meaningful commit message
-commit_message = "Generated Commit Message:\n\n" + diff[:500]  # Limiting to first 500 chars for brevity
+commit_message = "Generated Commit Message: Hi Bozo"  # Limiting to first 500 chars for brevity
 
 # Write the commit message to a file
 with open("commit_message.txt", "w") as f:
